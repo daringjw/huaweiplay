0135af0de5157f66333c23bd1304fdcb6b1f6d01
GuanJunwei <GuanJunwei@linux01>
2019-11-20 20:37:04 +0800

    huaweiplay

---
+++ Cn5AppStore/app/src/main/java/com/zb/cn5appstore/HomeActivity.java
@@ -0,0 +1,93 @@
+package com.zb.cn5appstore;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.widget.LinearLayout;
+
+import com.zb.cn5appstore.adapter.FixPagerAdapter;
+import com.zb.cn5appstore.base.BaseActivity;
+import com.zb.cn5appstore.base.BaseFragment;
+import com.zb.cn5appstore.factory.FragmentFactory;
+import com.zb.cn5appstore.fragment.RecommendFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * Created by VULCAN on 2019/11/17.
+ */
+
+public class HomeActivity extends BaseActivity {
+
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
+    @BindView(R.id.main_viewpager)
+    ViewPager mainViewPager;
+    @BindView(R.id.status_bar)
+    LinearLayout statusBar;
+
+
+    private FixPagerAdapter fixPagerAdapter;
+    private String[] titles = {"推荐", "分类", "排行", "管理", "我的"};
+    private List<Fragment> fragments;
+
+
+    @Override
+    protected void initLayout() {
+        setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    protected void initView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            final int statusHeight = getStatusBarHeight();
+            statusBar.post(new Runnable() {
+                @Override
+                public void run() {
+                    int titleHeight = statusBar.getHeight();
+                    android.widget.LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) statusBar.getLayoutParams();
+                    params.height = statusHeight + titleHeight;
+                    statusBar.setLayoutParams(params);
+                }
+            });
+        }
+
+        initViewPagerFragment();
+    }
+
+    private void initViewPagerFragment() {
+        fixPagerAdapter = new FixPagerAdapter(getSupportFragmentManager());
+
+        fragments = new ArrayList<>();
+        for (int i = 0; i < titles.length; i++) {
+            fragments.add(FragmentFactory.createFragment(i));
+        }
+        fixPagerAdapter.setTitles(titles);
+        fixPagerAdapter.setFragments(fragments);
+
+        mainViewPager.setAdapter(fixPagerAdapter);
+        tabLayout.setupWithViewPager(mainViewPager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+
+        mainViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                if (position ==0){
+                    RecommendFragment fragment = (RecommendFragment) FragmentFactory.createFragment(position);
+                    fragment.show();
+
+                }
+            }
+        });
+
+    }
+
+}
---
+++ Cn5AppStore/app/src/main/java/com/zb/cn5appstore/fragment/CategoryFragment.java
@@ -0,0 +1,28 @@
+package com.zb.cn5appstore.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zb.cn5appstore.R;
+import com.zb.cn5appstore.base.BaseFragment;
+
+/**
+ * Created by VULCAN on 2019/11/17.
+ */
+
+public class CategoryFragment extends BaseFragment{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false);
+
+    }
+
+
+}
